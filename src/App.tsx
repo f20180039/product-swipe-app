63d4c5b5e362b3d9f4336d833dff0755639cb5e4
Anshuman Singh <anshuman.singh@healthplix.com>
2025-05-03 12:36:31 +0530

    Project Setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className='mob-bg-Pink-100 mob-text-Black'>
+      a
+    </div>
+  )
+}
+
+export default App
